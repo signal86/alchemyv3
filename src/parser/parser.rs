6bcdcca1d731abd890e79ae400d2318b9811ef0c
signal86 <parathenium@proton.me>
2026-03-11 15:49:31 -0500

    broke

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -14,19 +14,24 @@ Node ->
     | ComponentInstance
     | Assignment
 
+Done
 Meta ->
     Keyword("meta") MetaSet Terminator(";")
 
+Done
 MetaSet ->
     Keyword("components")
     | Keyword("view")
 
+Done
 ComponentDefinition ->
     Keyword("create") Keyword("component") Operator(":") Identifier(*) OpenBrace("{") ComponentBody CloseBrace("}") Terminator(";")
 
+Done
 ComponentBody ->
     ComponentProperties*
 
+Done
 ComponentProperties ->
     VarsP
     | DefaultP
@@ -34,22 +39,28 @@ ComponentProperties ->
     | CssTemplateP
     | JSTemplateP
 
+Done
 VarsP ->
     Identifier("vars") Operator("=") OpenBracket("[") StrList CloseBracket["]"] Terminator(";")
 
+Done
 StrList ->
     String(*)
     | String(*) Separator(",") StrList
 
+Done
 DefaultP ->
     Identifier("default") Operator("=") String(*) Terminator(";")
 
+Done
 HtmlTemplateP ->
     Identifier("html") Operator("=") String(*) Terminator(";")
 
+Done
 CssTemplateP ->
     Identifier("css") Operator("=") String(*) Terminator(";")
 
+Done
 JSTemplateP ->
     Identifier("js") Operator("=") String(*) Terminator(";")
 
@@ -105,18 +116,6 @@ pub struct AST {
     pub nodes: Vec<ASTNode>,
 }
 
-#[derive(Debug, Copy, Clone)]
-pub enum ParserErrorType {
-    Example,
-}
-
-#[derive(Debug, Clone)]
-pub struct ParserError {
-    pub error_type: ParserErrorType,
-    pub msg: String,
-    pub line: u128,
-}
-
 /*
 Meta ->
     Keyword("meta") MetaSet Terminator(";")
@@ -126,23 +125,21 @@ MetaSet ->
     | Keyword("view")
 */
 
-// TODO: convert to expects
 #[allow(non_snake_case)]
 pub fn parse_MetaSet(lexer: &mut Lexer) -> Result<Meta, Error> {
-    match lexer.curr_token.t {
-        TokenType::Keyword => match lexer.curr_token.lexeme.as_str() {
-            "components" => {
-                lexer.consume_token();
-                Ok(Meta::Components)
-            }
-            "view" => {
-                lexer.consume_token();
-                Ok(Meta::View)
-            }
-            _ => Err(Error::new(ErrorKind::Other, "bad keyword")),
-        },
-        _ => Err(Error::new(ErrorKind::Other, "not a keyword")),
+    expect_token(lexer, TokenType::Keyword)?;
+    match lexer.curr_token.lexeme.as_str() {
+        "components" => {
+            lexer.consume_token();
+            Ok(Meta::Components)
+        }
+        "view" => {
+            lexer.consume_token();
+            Ok(Meta::View)
+        }
+        _ => Err(Error::new(ErrorKind::Other, "bad keyword")),
     }
+
     // if lexer.curr_token.t == TokenType::Keyword && lexer.curr_token.lexeme == "components" {
     //     Ok(Meta::Components)
     // } else if lexer.curr_token.t == TokenType::Keyword && lexer.curr_token.lexeme == "view" {
@@ -152,28 +149,33 @@ pub fn parse_MetaSet(lexer: &mut Lexer) -> Result<Meta, Error> {
     // }
 }
 
-// TODO: convert to expects
 #[allow(non_snake_case)]
 pub fn parse_Meta(lexer: &mut Lexer) -> Result<Meta, Error> {
-    match lexer.curr_token.t {
-        TokenType::Keyword => match lexer.curr_token.lexeme.as_str() {
-            "meta" => {
-                lexer.consume_token();
-                match parse_MetaSet(lexer) {
-                    Ok(s) => match lexer.curr_token.t {
-                        TokenType::Terminator => {
-                            lexer.consume_token();
-                            Ok(s)
-                        }
-                        _ => Err(Error::new(ErrorKind::Other, "terminator not found")),
-                    },
-                    Err(E) => Err(E),
-                }
-            }
-            _ => Err(Error::new(ErrorKind::Other, "bad keyword")),
-        },
-        _ => Err(Error::new(ErrorKind::Other, "not a keyword")),
-    }
+    expect(lexer, TokenType::Keyword, "meta")?;
+    lexer.consume_token();
+    let s = parse_MetaSet(lexer)?;
+    expect_token(lexer, TokenType::Terminator)?;
+    Ok(s)
+
+    // match lexer.curr_token.t {
+    //     TokenType::Keyword => match lexer.curr_token.lexeme.as_str() {
+    //         "meta" => {
+    //             lexer.consume_token();
+    //             match parse_MetaSet(lexer) {
+    //                 Ok(s) => match lexer.curr_token.t {
+    //                     TokenType::Terminator => {
+    //                         lexer.consume_token();
+    //                         Ok(s)
+    //                     }
+    //                     _ => Err(Error::new(ErrorKind::Other, "terminator not found")),
+    //                 },
+    //                 Err(E) => Err(E),
+    //             }
+    //         }
+    //         _ => Err(Error::new(ErrorKind::Other, "bad keyword")),
+    //     },
+    //     _ => Err(Error::new(ErrorKind::Other, "not a keyword")),
+    // }
 }
 
 /*
@@ -323,6 +325,45 @@ fn parse_ComponentDefinition(lexer: &mut Lexer) -> Result<ComponentDefinition, E
     lexer.consume_token();
     Ok(def)
 }
+
+/*
+ComponentInstance ->
+    Keyword("create") Identifier(*) Terminator(";")
+    | Keyword("create") Identifier(*) Operator(":") Identifier(*) Terminator(";")
+    | Keyword("create") Identifier(*) Operator(":") Identifier(*) Operator("=") String(*) Terminator(";")
+*/
+
+#[allow(non_snake_case)]
+fn parse_ComponentInstance(lexer: &mut Lexer) -> Result<ComponentInstance, Error> {
+    expect(lexer, TokenType::Keyword, "create")?;
+    lexer.consume_token();
+    expect_token(lexer, TokenType::Identifier)?;
+    let component = lexer.curr_token.lexeme;
+    let mut inst = ComponentInstance {
+        component: component,
+        identifier: None,
+        value: None,
+    };
+    lexer.consume_token();
+    match lexer.curr_token.t {
+        TokenType::Terminator => {
+            lexer.consume_token();
+            Ok(inst.clone())
+        }
+        TokenType::Operator => {
+            expect(lexer, TokenType::Operator, ":")?;
+            lexer.consume_token();
+            expect_token(lexer, TokenType::Identifier)?;
+            inst.name = lexer.curr_token.lexeme;
+        }
+        _ => Err(Error::new(ErrorKind::Other, "invalid component instance")),
+    }
+}
+
+/*
+Assignment ->
+    Identifier(*) Operator(".") Identifier(*) Operator("=") String(*) Terminator(";")
+*/
 
 // Wtf am i doing bro
 /*
